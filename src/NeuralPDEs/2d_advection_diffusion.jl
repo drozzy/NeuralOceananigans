26d448e7850bb6a6f2ee89d3d5f69a2869a7ee32
Andriy Drozdyuk <andriy@drozdyuk.com>
2021-01-12 20:26:36 -0500

    trying things

--- src/NeuralPDEs/2d_advection_diffusion.jl
+++ src/NeuralPDEs/2d_advection_diffusion.jl
@@ -22,19 +22,20 @@ u = 1.0
 v = 1.0
 v_vector = (u, v)
 R = 0
-D = 0.1 # diffusion
+D = 0 # 0.1 # diffusion
 t_max = 2.0
 x_min = -1.0
 x_max = 1.0
 y_min = -1.0
 y_max = 1.0
 
+# exp(-(x^2+y^2)/0.1)
 # div(v_vector * c) = dx(uc) + dy(vc)
 # Equations, initial and boundary conditions
 eqs = [ Dt(c(t,x,y)) ~ D * (Dxx(c(t,x,y)) + Dyy(c(t,x,y))) - (u*Dx(c(t,x,y)) + v*Dy(c(t,x,y))) + R]
 
 bcs = [ 
-        c(0, x, y) ~ cos(π*x) * cos(π*y) + 1.0,  
+        c(0, x, y) ~ exp(-(x^2+y^2)/0.1) + 1, #cos(π*x) * cos(π*y) + 1.0,  
         c(t, x_min, y) ~ c(t, x_max, y),
         c(t, x, y_min) ~ c(t, x, y_max)
 ]
@@ -74,7 +75,7 @@ cb = function (p,l)
     return false
 end
 
-res = GalacticOptim.solve(prob,ADAM();cb=cb,maxiters=100)
+res = GalacticOptim.solve(prob,ADAM();cb=cb,maxiters=50)
 
 
 # Plots
@@ -86,19 +87,22 @@ initθ = discretization.initθ
 acum =  [0;accumulate(+, length.(initθ))]
 sep = [acum[i]+1 : acum[i+1] for i in 1:length(acum)-1]
 minimizers = [res.minimizer[s] for s in sep]
-ts,xs = [domain.domain.lower:dx:domain.domain.upper for domain in domains]
+ts,xs,ys = [domain.domain.lower:dx:domain.domain.upper for domain in domains]
 
+# Plot initial condition
+c_predict = reshape([ phi([0, x, y], res.minimizer)[1] for x in xs for y in ys], length(xs), length(ys))
+plot(xs, ys, c_predict)
+
+# Animate
 anim = @animate for (i, t) in enumerate(0:dt:t_max)
     @info "Animating frame $i..."
-    c_predict = reshape([phi([t, x], res.minimizer)[1] for x in xs], length(xs))
+    c_predict = reshape([phi([t, x, y], res.minimizer)[1] for x in xs for y in ys], length(xs), length(ys))
     title = @sprintf("Advection-diffusion t = %.3f", t)
-    plot(xs, c_predict, label="", title=title , ylims=(0., 2))
+    heatmap(xs, ys, c_predict, label="", title=title , xlims=(-1, 1), ylims=(-1, 1), color=:thermal)#, clims=(0, 1))
 end
 
-gif(anim, "advection_diffusion_pinn.gif", fps=15)
+gif(anim, "advection_diffusion_2d_pinn.gif", fps=15)
 
-c_predict = reshape([ phi([0, x], res.minimizer)[1] for x in xs], length(xs))
-plot(xs,c_predict)
 
 # Plot correct solution
 using JLD2
